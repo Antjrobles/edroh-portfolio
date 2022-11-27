9f9afea74697c2f2b1fcf8e62f10b87d3279f3ab
Antonio Robles <antjrobles@gmail.com>
2022-11-27 07:21:02 +0100

    menu Image Profile added

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -1,5 +1,5 @@
 import useMediaQuery from "../hooks/useMediaQuery";
-import { motion } from = "framer-motion";
+import { motion } from  "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 
@@ -12,12 +12,14 @@ const Landing = ({setSeltectedPage}) => {
             {/*  IMAGE SECTION  */}
         <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
           {isAboveMediaScreens ? (
-            <div
-             className="relative "
-            >
+            <div 
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
+            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+                >
               <img  
               alt="profile"
-              src="assets/profile-img.png"
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+              src="../assets/profile-image.png"
               />
             </div>
           ) : (
